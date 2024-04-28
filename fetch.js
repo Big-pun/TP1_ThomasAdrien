989a9d8d8c07a7e855751acf8959a255d3441c88
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-04-28 19:42:28 -0400

    fichier .json pour remplacer API

---
+++ fetch.js
@@ -0,0 +1,105 @@
+// TODO API variables et requête
+
+const searchInput = document.getElementById("search__input");
+const searchButton = document.getElementById("search__btn");
+const clearButton = document.getElementById("clear__btn");
+
+
+
+const fetchData = async (searchName) => {
+    console.log("searchName")
+    try {
+        const response = await fetch('db.json');
+        if (!response.ok) throw new Error('La requête a rencontré un problème');
+        const data = await response.json();
+        if (data.characters.length === 0) throw new Error(`Aucun résultat pour le mot ${searchName}`);
+        const character = data.characters.find(character => character.name.toLowerCase() === searchName.toLowerCase());
+        if (!character) throw new Error(`Aucun personnage trouvé avec le nom ${searchName}`);
+        displayData(character);
+    } catch (error) {
+        displayErrorPopup(error.message);
+    }
+};
+
+
+    // TODO API affichage des erreurs
+
+    function displayErrorPopup(message) {
+        console.error(message); // Affiche le message d'erreur dans la console
+        alert(message); // Lance une fenêtre pop-up avec le message d'erreur
+    }
+
+    // TODO API affichage des résultats
+
+    function displayData(characters) {
+        console.log("displayData");
+
+        const data = characters[0];
+        const characterName = characters.name;
+        const characterBio = characters.bio;
+        const characterImage = characters.img;
+        const abilities = characters.abilities;
+
+        const resultNameElement = document.getElementById("result__name");
+        resultNameElement.textContent = characterName;
+
+        const resultBioElement = document.getElementById("result__bio");
+        resultBioElement.textContent = characterBio;
+
+        const resultImageElement = document.getElementById("result__img");
+        resultImageElement.src = characterImage;
+        resultImageElement.alt = characterName;
+
+        const resultAbilities = document.getElementById("result__abilities");
+        resultAbilities.innerText = abilities
+        }
+        
+
+    // TODO API effacer les résultats
+
+    function clearData() { // Fonction qui supprime la data précédente
+        console.log("clearData");
+
+        const resultNameElement = document.getElementById("result__name");
+        resultNameElement.textContent = "";
+
+        const resultBioElement = document.getElementById("result__bio");
+        resultBioElement.textContent = "";
+
+        const resultImageElement = document.getElementById("result__img");
+        resultImageElement.src = "";
+        resultImageElement.alt = "";
+
+        const resultAbilities = document.getElementById("result__abilities");
+        resultAbilities.innerHTML = "";
+
+        searchInput.value = "";
+
+    }
+
+        // TODO API recherche
+
+        function startSearch(event) {
+            console.log("startSearch");
+
+            const name = searchInput.value.toLowerCase();
+            if (name !== "") {
+                fetchData(name);
+            } else {
+                displayErrorPopup("Veuillez entrer un nom de personnage");
+            }
+        }
+
+
+        // TODO ajout event listener pour le click sur le search bouton et pour la pression de la touche enter du clavier
+
+        searchButton.addEventListener("click", startSearch);
+        searchInput.addEventListener("keypress", (event) => {
+            if (event.key === "Enter") {
+                startSearch(event);
+            }
+        });
+
+        // TODO appel de la fonction clearDefinition lors du click sur clearButton
+
+        clearButton.addEventListener("click", clearData);
--- script.js
+++ script.js
@@ -5,75 +5,36 @@ showSlides(slideIndex);
 
 // Suivant /Precedent
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+    showSlides((slideIndex += n));
 }
 
 // Image actuelle
 function currentSlide(n) {
-  showSlides((slideIndex = n));
+    showSlides((slideIndex = n));
 }
 
 // Affichage des images
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("carousel__slides"); // On récupère les images
-  let dots = document.getElementsByClassName("carousel__dot"); // On récupère les points
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  // On enlève la classe active
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex - 1].style.display = "block"; // On affiche l'image
-  dots[slideIndex - 1].className += " active"; // On ajoute la classe active
-}
-
-// TODO API variables et requête
-
-const searchInput = document.getElementsByClassName("search__input");
-const searchButton = document.getElementsByClassName("search__btn");
-const searchResults = document.getElementsByClassName("search__results");
-const clearButton = document.getElementsByClassName("clear__btn");
-
-const fetchData = async (name) => {
-    try {
-        console.log("fetchData");
-
-        const response = await fetch('db/db.json');
-        const data = await response.json();
-
-        if (!response.ok) {
-            throw new Error('La requête a rencontré un problème');
-        }
-
-    const character = data.characters.find(character => character.id === name);
-        console.log(character);
-
-        } catch (error) {
-
-            console.log("Erreur: ", error);
-        }
-
+    let i;
+    let slides = document.getElementsByClassName("carousel__slides"); // On récupère les images
+    let dots = document.getElementsByClassName("carousel__dot"); // On récupère les points
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
 
-    // TODO API recherche
+    // On enlève la classe active
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
 
-    function startSearch(event) {
-        console.log("startSearch");
-      
-        const name = searchInput.value.trim();
-        if (name !== "") {
-          fetchData(name);
-        } else {
-          displayErrorPopup("Veuillez entrer un nom de personnage");
-        }
-      }
+    slides[slideIndex - 1].style.display = "block"; // On affiche l'image
+    dots[slideIndex - 1].className += " active"; // On ajoute la classe active
+}
+
+
